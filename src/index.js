45f1d36107f38431a2e10ab6364fec3a5633a0c1
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2023-10-19 15:34:49 +0300

    fix Routes

--- src/index.js
+++ src/index.js
@@ -4,18 +4,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import { Provider } from 'react-redux';
-import store from './store/store';
+// import { Provider } from 'react-redux';
+// import store from './redux-store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-       <App />
-
-      <Provider store={store}>
+    <BrowserRouter  basename="/Chat_Project">
+      {/* <Provider store={store}> */}
         <App />
-      </Provider>
+      {/* </Provider> */}
     </BrowserRouter>
   </React.StrictMode>
 );
