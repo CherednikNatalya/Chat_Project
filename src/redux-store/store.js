18f7b0e93dac1e1eeb904d573648544754784ec7
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2023-10-26 13:34:31 +0300

    fix settings

--- src/redux-store/store.js
+++ src/redux-store/store.js
@@ -11,8 +11,7 @@ import {
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { authSlice } from 'redux-store/AuthOperations/slice';
-// import { tasksSlice } from './tasks/tasksSlice';
-// import { reviewsSlice } from './Reviews/reviewsSlice'
+
 
 const authPersistConfig = {
   key: 'auth',
@@ -23,8 +22,7 @@ const authPersistConfig = {
 export const store = configureStore({
   reducer: {
     auth: persistReducer(authPersistConfig, authSlice.reducer),
-    // tasks: tasksSlice.reducer,
-    // reviews: reviewsSlice.reducer,
+    
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
