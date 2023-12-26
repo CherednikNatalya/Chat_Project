09ed9fc333c28efc306bb3543657201cab809129
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2023-12-26 10:07:55 +0200

    add

--- src/redux-store/AuthOperations/AuthOperations.js
+++ src/redux-store/AuthOperations/AuthOperations.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import swal from 'sweetalert';
 
+
+
 axios.defaults.baseURL ='https://talk-and-travel.pp.ua';
 
 export const token = {
